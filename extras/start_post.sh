4803c28701c295972d46cfaf6358345738df0ef7
Eric C. Anderson <eric.anderson@noaa.gov>
2014-10-05 21:06:20 -0700

    Add functionality for posting and listing blogs.

---
+++ extras/start_post.sh
@@ -0,0 +1,26 @@
+# call this with the quoted blog post title as the only argument
+
+TITLE=$1
+title=$(echo $TITLE | sed 's/  */-/g; s/[^-0-9a-zA-Z_]//g' | awk '{print tolower($1)}')
+thedate=$(date "+%Y-%m-%d")
+filename=_posts/${thedate}-$title.rmd
+
+if [ -e $filename ]; then
+  echo "$filename already exists"
+  echo "Exiting..."
+  exit 1
+fi
+
+
+echo "---
+title: $TITLE
+layout: default_with_disqus
+output: bookdown::html_chapter
+---
+
+# $TITLE ($thedate)
+
+
+" > $filename
+
+
